9ed0cf60f64cd82e77b440b772e87f14d6e7535e
Andres Cabada <cm_andres@hotmail.com>
2025-05-22 23:49:00 -0600

    Prev Go to Live

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 // Array con las URLs de las imágenes del carrusel
 const images = [
-    "https://picsum.photos/200/300?random=1",
-    "https://picsum.photos/200/300?random=2",
-    "https://picsum.photos/200/300?random=3",
-    "https://picsum.photos/200/300?random=4"
+    "images/Mr. Coffee main.png",
+    "images/Mr. Coffee 1.png",
+    "images/Mr. Coffee 2.png",
+    "images/Mr. Coffee 3.png",
+    "images/Mr. Coffee 4.png"
 ];
 
 // Selección de elementos
@@ -23,3 +24,25 @@ function moveSlide(step) {
     changeImage(currentIndex);
 }
 
+/* promociones */
+const modal = document.getElementById('img-modal');
+const modalImg = document.getElementById('modal-img');
+const closeBtn = document.querySelector('.modal-close');
+
+document.querySelectorAll('#promo-gallery img').forEach(img => {
+  img.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+modal.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
